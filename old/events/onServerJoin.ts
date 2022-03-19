4be202e5878857b0c41c535e8a6f02b3bc151c6d
Wyzedix <wyzedix@gmail.com>
2022-03-19 19:48:39 +0100

    Goodbye old code (still on main branch for now)

--- old/events/onServerJoin.ts
+++
@@ -1,9 +0,0 @@
-import {Guild} from "discord.js"
-
-const eventLoader = (log: (t: string, m: string | Error) => void) => {
-	return (server: Guild) => {
-		log("INVITED", `Epsibot has joined a new server: ${server.name}`)
-	}
-}
-
-export default eventLoader
