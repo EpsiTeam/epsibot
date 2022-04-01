f1999885c4554d3ec342820006fb44e05fd34195
Wyzedix <wyzedix@gmail.com>
2022-04-01 19:44:51 +0200

    New help for command args

--- src/command/GuildCommand/helper.ts
+++ src/command/GuildCommand/helper.ts
@@ -3,15 +3,22 @@ import { CustomCommand } from "../../entity/CustomCommand.js";
 import { CustomEmbedCommand } from "../../entity/CustomEmbedCommand.js";
 import { EpsibotColor } from "../../utils/color/EpsibotColor.js";
 
-export function helpArgument(name: string) {
-	return `
+export const helpArgument = `
 
-	Il est possible de spécifier des arguments avec \`$\` suivi du numéro de l'argument, la numérotation commence à 0
-	__Exemple__: si la réponse de la commande est \`Bonjour $0\`, faire la commande \`${name} Paul\` répondra \`Bonjour Paul\`
+**Inutile de lire la suite de ce message si la commande ne doit pas contenir de paramètres**
 
-	Pour mettre plusieurs mots dans un seul argument d'une commande, il faut entourer les mots de \`'\` ou \`"\`
-	__Exemple__: si la réponse de la commande est \`Bojour $0 et $1\`, faire la commande  \`${name} "à tous" bienvenue\` affichera \`Bonjour à tous et bienvenue\``;
-}
+Il est possible de spécifier des paramètres de commande avec \`$\` suivi du numéro de l'argument (la numérotation commence à 0)
+__Exemple__:
+Imaginons une commande \`!bonjour\` qui répond \`Bonjour $0\`
+Faire la commande \`!bonjour Paul\` répondra \`Bonjour Paul\`
+(\`$0\` sera remplacé par \`Paul\`)
+Faire la commande \`!bonjour Paul Pierre Jacques\` répondra aussi \`Bonjour Paul\` (les paramètres en trop sont supprimés)
+
+Pour mettre plusieurs mots dans un seul paramètre d'une commande, il faut entourer les mots de \`'\` ou \`"\`
+__Exemple__:
+Imaginons une commande \`!bonjour\` qui répond \`Bonjour $0, n'oubliez pas $1 !\`
+Faire la commande  \`!bonjour 'à tous' "de boire de l'eau"\` affichera \`Bonjour à tous, n'oubliez pas de boire de l'eau\`
+(\`$0\` sera remplacé par \`à tous\`, et \`$1\` sera remplacé par \`de boire de l'eau\`)`;
 
 export function timeoutEmbed(name: string): InteractionReplyOptions {
 	return {
