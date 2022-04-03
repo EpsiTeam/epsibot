60d8e1cb90c479ea5b9e67aaf7086f2e57f9ba2f
Wyzedix <wyzedix@gmail.com>
2022-04-03 15:38:34 +0200

    Shifumi leaderboard is here

--- src/command/Shifumi/Shifumi.ts
+++ src/command/Shifumi/Shifumi.ts
@@ -1,26 +1,50 @@
 import { CommandInteraction } from "discord.js";
 import { Command } from "../Command.js";
+import { leaderboard } from "./leaderboard.js";
 import { play, PlayParam } from "./play.js";
+
+enum Subcommand {
+	play = "play",
+	leaderboard = "leaderboard"
+}
 
 export class Shifumi extends Command {
 	constructor() {
-		super("shifumi", "Lance un shifumi contre quelqu'un (pierre feuille ciseau)");
+		super("shifumi", "Permet de joueur au shifumi");
 
 		this.options = [{
-			type: "USER",
-			name: PlayParam.user,
-			description: "L'utilisateur contre lequel vous souhaitez jouer",
-			required: true
+			type: "SUB_COMMAND",
+			name: Subcommand.play,
+			description: "Lance une partie de shifumi contre quelqu'un",
+			options: [{
+				type: "USER",
+				name: PlayParam.user,
+				description: "L'utilisateur contre lequel vous souhaitez jouer",
+				required: true
+			}, {
+				type: "NUMBER",
+				name: PlayParam.turnsToWin,
+				description: "Le nombre de tours qu'un joueur doit gagner pour gagner la partie (3 par défaut)",
+				minValue: 1,
+				required: false
+			}]
 		}, {
-			type: "NUMBER",
-			name: PlayParam.turnsToWin,
-			description: "Le nombre de tours qu'un joueur doit gagner pour gagner la partie (3 par défaut)",
-			minValue: 1,
-			required: false
+			type: "SUB_COMMAND",
+			name: Subcommand.leaderboard,
+			description: "Voir le tableau des scores du shifumi"
 		}];
 	}
 
 	async execute(interaction: CommandInteraction<"cached">) {
-		return play(interaction);
+		const subcommand = interaction.options.getSubcommand();
+
+
+		if (subcommand === Subcommand.play)
+			return play(interaction);
+
+		if (subcommand === Subcommand.leaderboard)
+			return leaderboard(interaction);
+
+		throw Error(`Unknown subcommand ${subcommand}`);
 	}
 }
