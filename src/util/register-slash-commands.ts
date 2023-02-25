bef39955b920536bf3f6d199e4ad687c18f5097d
Wyzedix <wyzedix@gmail.com>
2023-02-25 13:47:02 +0100

    added script to clear slash commands

--- src/util/register-slash-commands.ts
+++ src/util/register-slash-commands.ts
@@ -8,12 +8,18 @@ import { CommandManager } from "../command/CommandManager.js";
 import { EnvVariable } from "./EnvVariable.js";
 import { Logger } from "./Logger.js";
 
+function getRest() {
+	return new REST({ version: "10" }).setToken(EnvVariable.discordToken);
+}
+
 async function registerOnGuild(
 	guildId: Snowflake,
 	commands: RESTPostAPIChatInputApplicationCommandsJSONBody[],
 	rest: REST
 ) {
-	Logger.info(`Registering ${commands.length} commands on guild ${guildId}`);
+	Logger.info(
+		`Registering ${commands.length} slash commands on guild ${guildId}`
+	);
 
 	return rest.put(
 		Routes.applicationGuildCommands(EnvVariable.applicationId, guildId),
@@ -27,23 +33,49 @@ async function registerGlobally(
 	commands: RESTPostAPIChatInputApplicationCommandsJSONBody[],
 	rest: REST
 ) {
-	Logger.info(`Registering ${commands.length} commands globally`);
+	Logger.info(`Registering ${commands.length} slash commands globally`);
 
 	return rest.put(Routes.applicationCommands(EnvVariable.applicationId), {
 		body: commands
 	});
 }
 
-export async function registerCommands() {
+export async function registerSlashCommands() {
 	const manager = new CommandManager();
 	const commands = manager.commandList.map((command) =>
 		command.buildCommandData()
 	);
-	const rest = new REST({ version: "10" }).setToken(EnvVariable.discordToken);
+	const rest = getRest();
 
 	if (EnvVariable.production) {
 		return registerGlobally(commands, rest);
 	}
 
 	return registerOnGuild(EnvVariable.devGuildId, commands, rest);
+}
+
+export async function clearSlashCommands() {
+	const rest = getRest();
+
+	if (EnvVariable.production) {
+		Logger.info(`Clearing all global slash commands`);
+
+		return rest.put(Routes.applicationCommands(EnvVariable.applicationId), {
+			body: []
+		});
+	}
+
+	Logger.info(
+		`Clearing all slash commands on guild ${EnvVariable.devGuildId}`
+	);
+
+	return rest.put(
+		Routes.applicationGuildCommands(
+			EnvVariable.applicationId,
+			EnvVariable.devGuildId
+		),
+		{
+			body: []
+		}
+	);
 }
